d741151bc1170f7bbf081f5fe1f53aba938cdd3c
Jahongir-veb <jahongirmh@gmail.com>
2021-01-30 23:47:12 +0500

    added header site

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// alert(`hi`);
+elMenuBtn = document.querySelector(`.header-site-nav__toggle-btn`);
+elMenu = document.querySelector(`.header-site-nav__menu-list`);
+
+
+elMenuBtn.addEventListener(`click`, function(){
+  elMenuBtn.classList.toggle(`is-active`);
+  elMenu.classList.toggle(`active`);
+})
