2521bbe2139197ba8b72d37461a3391ef75766d0
Jahongir-veb <jahongirmh@gmail.com>
2021-02-02 12:09:55 +0500

    changes footer date style

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-elMenuBtn = document.querySelector(`.header-site-nav__toggle-btn`);
-elMenu = document.querySelector(`.header-site-nav__menu-list`);
-
+let elMenuBtn = document.querySelector(`.header-site-nav__toggle-btn`);
+let elMenu = document.querySelector(`.header-site-nav__menu-list`);
+let elFooterDateSpan = document.querySelector(`.footer-site__span`);
 
 elMenuBtn.addEventListener(`click`, function(){
   elMenuBtn.classList.toggle(`is-active`);
   elMenu.classList.toggle(`active`);
 })
+
+// Get the current year for the copyright
+elFooterDateSpan.textContent = new Date().getFullYear();
